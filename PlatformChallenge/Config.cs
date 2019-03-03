047b334145cc6f317983590491fc9ef5821bbb4c
PhilPelton <48092398+PhilPelton@users.noreply.github.com>
2019-03-03 22:47:16 +0000

    Change config to use original input file

--- PlatformChallenge/Config.cs
+++ PlatformChallenge/Config.cs
@@ -2,7 +2,7 @@
 {
     class Config
     {
-        public const string csvFileLocation = @"C:\Users\Phil\Documents\GitHub\gt-platform-challenge\squeaksClean.csv";
+        public const string csvFileLocation = @"C:\Users\Phil\Documents\GitHub\gt-platform-challenge\squeaks.csv";
 
         public const string webHookURL = @"https://webhook.site/5d64bef9-2742-43b7-8856-f9efcf5b3a56";
 
